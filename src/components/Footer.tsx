d347fecd1484fed404ce4c3bf12fb8f598165d3b
mikesosa <michael@buydepa.com>
2023-01-16 12:03:31 -0500

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,10 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2">
-      <p className="text-gray-500 text-xs font-mono">Creado con ❤️ por</p>
+    <div className="absolute top-5 left-1/2 transform -translate-x-1/2">
+      <p className="text-gray-500 text-sm font-mono">
+        Creado con <span className="text-xs">🤍</span> por
+      </p>
       <div className="font-bold text-gray-300 text-xs font-mono text-center mt-2">
         <Link href="https://twitter.com/micael_sosa">michaelsosa</Link>
       </div>

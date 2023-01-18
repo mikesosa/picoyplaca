96964230305d2d81cb7c842aff9fd44a81288833
mikesosa <michael@buydepa.com>
2023-01-17 20:46:08 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
     const day = date.slice(6, 8);
     const isDateEven = Number(day) % 2 === 0;
     const isPlateEven = EVEN_PLATES.includes(Number(lastDigit));
-    const restrictionDays = getRestrictionDates(isPlateEven, weekDates);
+    const restrictionDays = getRestrictionDates(!isPlateEven, weekDates);
     if (isDateEven) {
       setResult(ODD_PLATES.includes(Number(lastDigit)));
     } else {
